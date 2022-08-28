f4429bcf83f71b4dfae8dc4885e2cdfe985e6d43
olala3 <olga_tsyrulyk@outlook.com>
2022-08-28 16:49:46 +0800

    Added the search engine

--- src/script.js
+++ src/script.js
@@ -43,8 +43,19 @@ function displayTemperature(response) {
   );
 }
 
-let apiKey = "c16458f8a4b1182988c92b5eb498190c";
-let city = "Shanghai";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "c16458f8a4b1182988c92b5eb498190c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
