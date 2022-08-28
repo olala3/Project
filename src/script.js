227c15ac078beb653ec642d7d26dad19aa4545e0
olala3 <olga_tsyrulyk@outlook.com>
2022-08-28 15:19:38 +0800

    Showing the current temperature

---
+++ src/script.js
@@ -0,0 +1,19 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "c16458f8a4b1182988c92b5eb498190c";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Jiaxing&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
